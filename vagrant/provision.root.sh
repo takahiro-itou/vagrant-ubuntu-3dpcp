0e1ae31fe6a6cdc92aa8f38e69c5e3fa9d2b2233
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-06-25 09:16:29 +0900

    不要な Docker の設定は削除

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -25,23 +25,4 @@ echo  -e  "tmpfs\t/ramdisk\ttmpfs\trw,size=2048m,x-gvfs-show\t0\t0"  \
 
 sudo  mount  -a
 
-# Docker
-
-cat  << __EOF__  |  sudo  tee  /etc/docker/daemon.json
-{
-    "data-root" : "/ext-hdd/data/docker"
-}
-__EOF__
-
-ps  aux  | grep  docker
-rsync  -av  /var/lib/docker/  /ext-hdd/data/docker/
-mv  /var/lib/docker  /var/lib/docker.org
-ln  -s   /ext-hdd/data/docker  /var/lib/docker
-rm  -rf  /var/lib/docker.org
-
-sudo  systemctl  restart  docker
-
-sudo  chmod  1777  /ext-hdd/data
-sudo  gpasswd  -a  vagrant  docker
-
 date  >  /root/.provision.root
